125803dfbb4af8786a0781006d20fef17cbeb3e0
HaoxuanWang_OldB <56232866+Thomaswang0822@users.noreply.github.com>
2021-05-03 00:08:06 +0800

    halfway done

--- script.js
+++ script.js
@@ -2,14 +2,45 @@
 
 const img = new Image(); // used to load image from <input> and draw to canvas
 
+// new consts here:
+const canvas = document.getElementById('user-image');
+const ctx = canvas.getContext('2d');
+var imageInput = document.getElementById("image-input");
+// buttons to take care
+const clear = document.querySelector("[type='reset']");
+const read = document.querySelector("[type='button']");
+const volume = document.getElementById('voice-selection');
+volume.disabled = false;
+const submit = document.querySelector("[type='submit']"); // also need to turn submit off.
+
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
-  // TODO
+  
+  canvas.clearRect(0,0, canvas.width, canvas.height);
+  ctx.fillStyle = 'black';
+  
+  submit.disabled = false;
+  clear.disabled = true;
+  read.disabled = true;
+  
 
+  ctx.fillRect(0,0, canvas.width, canvas.height);
+  var dims = getDimmensions(canvas.width, canvas.height, img.width, img.height);
+  ctx.drawImage(img, dims.startX, dims.startY, dims.width, dims.height);
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
+});
+
+
+// image-input on change
+img.addEventListener('change', () => {
+    // load in the selected image into the Image object (img) src attribute
+    img.src = URL.createObjectURL(imageInput.files[0]);
+
+    // set the image alt attribute by extracting the image file name from the file path
+    img.alt = img.src.split('\\').pop().split('/').pop();
 });
 
 /**
